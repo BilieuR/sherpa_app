47f3e41fdd3b8e79ec8d2206eb2d0e934becf196
BilieuR <bilieureath1@gmail>
2017-11-02 11:24:40 +1000

    Ive lost count

--- smartcity/settings.py
+++ smartcity/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
--- smartcity/wsgi.py
+++ smartcity/wsgi.py
@@ -16,5 +16,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smartcity.settings")
 from whitenoise.django import DjangoWhiteNoise
 
 
-# application = get_wsgi_application()
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
